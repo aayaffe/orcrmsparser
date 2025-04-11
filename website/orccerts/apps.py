c75a198189887f7f0142de38de262e8de330bc4c
Amit Yaffe <aayaffe@gmail.com>
2025-04-11 21:10:38 +0300

    Remove unused Django application 'orccerts' and related files, including views, models, templates, and settings. Update .gitignore to exclude 'orcsc/output/*'.

--- website/orccerts/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class OrccertsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'orccerts'
--- website/website/views.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-from django.shortcuts import render
-sys.path.append(os.getcwd() + '/..')
-
-
-def index(request):
-    return render(request, 'index.html')
