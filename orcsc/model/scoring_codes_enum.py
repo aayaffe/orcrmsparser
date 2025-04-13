076dbfe5ab94ce54d6ea0d8f2592211101ba5055
Amit Yaffe <aayaffe@gmail.com>
2025-04-13 21:13:50 +0300

    Remove commented-out scoring codes from ScoringCode enum

--- orcsc/model/scoring_codes_enum.py
+++ orcsc/model/scoring_codes_enum.py
@@ -10,6 +10,3 @@ class ScoringCode(Enum):
     TOT_Coastal_Long_distance = "TMF_Offshore"
     TOT_Windward_Leeward = "TMF_Inshore"
     TOT_All_Purpose = "APHT"
-    # TOT_Triple_Number_All_Purpose_Low = "US_TNAP_L_TOT"
-    # TOT_Triple_Number_All_Purpose_Medium = "US_TNAP_M_TOT"
-    # TOT_Triple_Number_All_Purpose_High = "US_TNAP_H_TOT"
